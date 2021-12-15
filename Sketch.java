38f56aca8bf51c3722a9485a70bb5bc6cadd6bfe
mrfabroa <eric.fabroa@ycdsbk12.ca>
2021-12-15 09:12:27 -0500

    animation starter

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,22 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+
+
+  /**
+   * state global variables
+   *  
+   * */ 
+  public float circleY = -50;
+
 	
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+	  // put your size call here
+    size(200, 200);
   }
 
   /** 
@@ -24,13 +32,22 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // clear out old frames
+    background(32);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // draw current frame based on state
+    ellipse(100, circleY, 50, 50);
+  
+    // modify state
+    circleY = circleY + 1;
+  
+    // reset state
+    if(circleY > height+50) {
+      circleY = 0;
+    }
   }
   
   // define other methods down here.
+
+
 }
